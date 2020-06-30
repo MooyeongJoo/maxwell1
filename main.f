f344b26b6f5356039dc3c493df6bfc6bfa43c904
MooyeongJoo <63994620+MooyeongJoo@users.noreply.github.com>
2020-06-30 14:05:25 +0900

    fixed

--- main.f
+++ main.f
@@ -1,8 +1,7 @@
       program main
       implicit none
-      real dl,eta,l,t,dt,sigma,c,f,ee,calc_f,tol,l0
-      integer kount,i
-      parameter(tol=1e-5)
+      real dl,eta,l,t,dt,sigma,c,f,ee,calc_f,l0
+      integer i
       l0=10.
 	  l=10.
       t=0.
@@ -13,23 +12,16 @@
       ee=200000000000.
 
 
-
       open(1,file='output.txt')
 
       do while(t<30.01)
       dl=c*dt
-      f=tol*2
-      kount=0
-      do while(abs(f)>tol.and.kount<10)
-        f=calc_f(sigma,eta,ee,c,t,dt,l0,l)
-        sigma=sigma+f
-        kount=kount+1
-      enddo
-
-      write(1,*)t,l,sigma,kount
+      f=calc_f(sigma,eta,ee,c,t,dt,l0,l)
+      sigma=f
+      write(1,*)t,l,sigma
       l=l+dl
-	  
       t=t+dt
       enddo
+	  
       close(1)
       end
